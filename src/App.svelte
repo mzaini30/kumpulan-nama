83b85b2974da1d36fa9c15ab23b6dd1a419a205e
mzaini30 <muhzaini30@gmail.com>
2024-01-01 22:57:10 +0800

    upload

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Login from "login-zen";
+  import { fakerID_ID as faker } from "@faker-js/faker";
+
+  let listNama = [];
+
+  function generateListNama() {
+    listNama = [];
+    for (let x = 1; x <= 20; x++) {
+      listNama = [...listNama, faker.person.fullName()];
+    }
+  }
+  generateListNama();
+  setInterval(generateListNama, 5000);
+</script>
+
+<svelte:head>
+  <title>Kumpulan Nama</title>
+</svelte:head>
+
+<Login platform="Kumpulan Nama"></Login>
+
+<div class="p-3">
+  <ol>
+    {#each listNama as nama}
+      <li class="list-decimal ml-5">{nama}</li>
+    {/each}
+  </ol>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+    "./node_modules/login-zen/**/*.svelte"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
